7b82bbb917132f9e789c9208212d98c62c58873a
kristi <kristi.stanescu@gmail.com>
2018-10-18 08:52:34 +0300

    added dots

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,9 @@ const Header = ({ addTodo }) => (
     <h1><a href="https://www.reddit.com/user/D2cookie">/r/D2cookie</a> last hit challenges</h1>
   <header className="header">
     <div>
-      <p>All the challanges are done in lobby games, no cheats, no bots, 1v0</p>
+      <p>All the challanges are done in lobby games, no cheats, no bots, 1v0.</p>
       <p>There are no additional rules - unless stated in the challenge itself.</p>
-      <p>YES, they are all possible, just hard</p>
+      <p>YES, they are all possible, just hard.</p>
     </div>
   </header>
   </div>
